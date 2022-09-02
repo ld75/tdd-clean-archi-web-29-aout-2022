ee686b6e4b5606939ee6580a27596235c3785fcb
laurent <kanailaurent@gmail.com>
2022-09-02 20:38:18 +0200

    typage de dependances pour notre usecase

--- src/who-wants-to-be-millionaire/hexagon/use-cases/pickQuestionUsecase.ts
+++ src/who-wants-to-be-millionaire/hexagon/use-cases/pickQuestionUsecase.ts
@@ -1,11 +1,11 @@
-import {ReduxStore} from "../../store/reduxStore";
+import {Dependencies, ReduxStore} from "../../store/reduxStore";
 import {ThunkDispatch} from "@reduxjs/toolkit"
 import {QuestionGateway} from "../gateways/questionGateway"
 import {Question} from "../entities/question";
 import {AppThunk} from "../../store/reduxStore";
 
 export const pickQuestionUsecase=
-    ():AppThunk<Promise<void>>=>async (dispatch,getState,dependencies)=>{
-    const question:Question = await dependencies.questionGatewayInstance.getFirstQuestion()
+    ():AppThunk<Promise<void>>=>async (dispatch,getState,{questionGatewayInstance}:Dependencies)=>{
+    const question:Question = await questionGatewayInstance.getFirstQuestion()
     dispatch({type:"PICK_RETURNED_QUESTION",payload:{question}})
     }
